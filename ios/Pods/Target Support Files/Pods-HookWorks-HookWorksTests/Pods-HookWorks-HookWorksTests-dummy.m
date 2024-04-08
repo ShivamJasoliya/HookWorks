c3fc7f19c28e1adf4dfcd13ba9ad4ec73138a815
Shivam Jasoliya <50066554+ShivamJasoliya@users.noreply.github.com>
2024-04-08 23:15:04 +0530

    Delete ios/Pods directory

--- ios/Pods/Target Support Files/Pods-HookWorks-HookWorksTests/Pods-HookWorks-HookWorksTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HookWorks_HookWorksTests : NSObject
-@end
-@implementation PodsDummy_Pods_HookWorks_HookWorksTests
-@end
--- ios/Pods/Target Support Files/Pods-HookWorks/Pods-HookWorks-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HookWorks : NSObject
-@end
-@implementation PodsDummy_Pods_HookWorks
-@end
